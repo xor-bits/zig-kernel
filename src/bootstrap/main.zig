e02b227de5336e6ccca692d3bc5b2960e23a98af
Eemeli <oneleh@utu.fi>
2024-11-22 17:52:50 +0200

    buffered user space writer

--- src/bootstrap/main.zig
+++ src/bootstrap/main.zig
@@ -1,9 +1,57 @@
-export fn _start() linksection(".text._start") callconv(.C) noreturn {
-    log("hello world");
+const std = @import("std");
+
+const log = std.log.scoped(.bootstrap);
+
+//
+
+pub const std_options: std.Options = .{
+    .logFn = logFn,
+};
+
+fn logFn(comptime message_level: std.log.Level, comptime scope: @TypeOf(.enum_literal), comptime format: []const u8, args: anytype) void {
+    const level_txt = comptime message_level.asText();
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    var bw = std.io.bufferedWriter(SysLog{});
+    const writer = bw.writer();
+
+    nosuspend {
+        writer.print(level_txt ++ prefix2 ++ format ++ "\n", args) catch return;
+        bw.flush() catch return;
+    }
+}
+
+pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
+    _ = error_return_trace;
+
+    if (ret_addr) |at| {
+        std.log.scoped(.panic).err("panicked at 0x{x}:\n{s}", .{ at, msg });
+    } else {
+        std.log.scoped(.panic).err("panicked:\n{s}", .{msg});
+    }
+
     while (true) {}
 }
 
-pub fn log(s: []const u8) void {
+//
+
+export fn _start() linksection(".text._start") callconv(.C) noreturn {
+    log.info("hello world", .{});
+    while (true) {}
+}
+
+const SysLog = struct {
+    pub const Error = error{};
+    pub fn write(self: @This(), bytes: []const u8) Error!usize {
+        try self.writeAll(bytes);
+        return bytes.len;
+    }
+    pub fn writeAll(_: @This(), bytes: []const u8) Error!void {
+        sys_log(bytes);
+    }
+    pub fn flush(_: @This()) Error!void {}
+};
+
+pub fn sys_log(s: []const u8) void {
     _ = syscall2(1, @intFromPtr(s.ptr), s.len);
 }
 
