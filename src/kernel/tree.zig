7c3c352cff901bdd8c0842844e0a1e66c1203288
Eemeli <oneleh@utu.fi>
2024-12-01 16:43:33 +0200

    red-black tree with insert

---
+++ src/kernel/tree.zig
@@ -0,0 +1,228 @@
+const std = @import("std");
+
+//
+
+pub fn defaultOrder(comptime K: type) fn (K, K) std.math.Order {
+    return struct {
+        fn inner(a: K, b: K) std.math.Order {
+            return std.math.order(a, b);
+        }
+    }.inner;
+}
+
+pub fn RbTree(
+    comptime K: type,
+    comptime V: type,
+    comptime order: fn (K, K) std.math.Order,
+) type {
+    return struct {
+        root: ?*Node = null,
+
+        const Self = @This();
+
+        pub const Node = struct {
+            child: [2]?*Node,
+            parent: ?*Node,
+            color: Color,
+            key: K,
+            value: V,
+
+            fn dir(self: *Node) Dir {
+                if (self == self.parent.?.child[1]) {
+                    return .right;
+                } else {
+                    return .left;
+                }
+            }
+
+            fn color(node: ?*Node) Color {
+                if (node) |_node| {
+                    return _node.color;
+                }
+                return .black;
+            }
+        };
+
+        fn rotateDir(self: *Self, subtree: *Node, dir: Dir) ?*Node {
+            const g = subtree.parent;
+            const s = subtree.child[1 - @intFromEnum(dir)].?;
+            const c = s.child[@intFromEnum(dir)];
+
+            subtree.child[1 - @intFromEnum(dir)] = c;
+            if (c) |_c| {
+                _c.parent = subtree;
+            }
+
+            s.child[@intFromEnum(dir)] = subtree;
+            subtree.parent = s;
+            s.parent = g;
+
+            if (g) |_g| {
+                _g.child[if (subtree == _g.child[1]) 1 else 0] = s;
+            } else {
+                self.root = s;
+            }
+
+            return s;
+        }
+
+        pub fn debug(self: *Self) void {
+            _debug(self.root, 0);
+        }
+
+        fn _debug(node: ?*Node, lvl: usize) void {
+            std.log.info("{d}:", .{lvl});
+            if (node) |_node| {
+                std.log.info(" {any}", .{_node.key});
+                _debug(_node.child[0], lvl + 1);
+                _debug(_node.child[1], lvl + 1);
+            } else {
+                std.log.info(" null", .{});
+            }
+        }
+
+        pub fn get(self: *Self, key: K) ?*Node {
+            switch (self.entry(key)) {
+                .occupied => |node| {
+                    return node;
+                },
+                .vacant => {
+                    return null;
+                },
+            }
+        }
+
+        pub fn insert(self: *Self, inserting: *Node) ?*Node {
+            switch (self.entry(inserting.key)) {
+                .occupied => |node| {
+                    std.mem.swap(K, &node.key, &inserting.key);
+                    std.mem.swap(V, &node.value, &inserting.value);
+                    return inserting;
+                },
+                .vacant => |node| {
+                    self.insert1(inserting, node.parent, node.dir);
+                    return null;
+                },
+            }
+        }
+
+        pub const Entry = union(enum) {
+            occupied: *Node,
+            vacant: struct {
+                parent: ?*Node,
+                dir: Dir,
+            },
+        };
+
+        pub fn entry(self: *Self, key: K) Entry {
+            var parent: *Node = self.root orelse {
+                return Entry{ .vacant = .{
+                    .parent = null,
+                    .dir = .left,
+                } };
+            };
+
+            while (true) {
+                std.log.info("next lvl", .{});
+                const dir = dirFromOrder(order(key, parent.key)) orelse {
+                    return Entry{ .occupied = parent };
+                };
+                parent = parent.child[@intFromEnum(dir)] orelse {
+                    return Entry{ .vacant = .{
+                        .parent = parent,
+                        .dir = dir,
+                    } };
+                };
+            }
+        }
+
+        fn insert1(
+            self: *Self,
+            _node: *Node,
+            new_parent: ?*Node,
+            _dir: Dir,
+        ) void {
+            var node = _node;
+            var dir = _dir;
+            var grandparent: *Node = undefined;
+            var uncle: ?*Node = undefined;
+
+            node.color = .red;
+            node.child[0] = null;
+            node.child[1] = null;
+            node.parent = new_parent;
+
+            var parent: *Node = new_parent orelse {
+                self.root = node;
+                return;
+            };
+            parent.child[@intFromEnum(dir)] = node;
+
+            // rebalance
+            while (true) {
+                if (Node.color(parent) == .black) {
+                    // case i1
+                    return;
+                }
+
+                grandparent = parent.parent orelse {
+                    // case i4
+                    parent.color = .black;
+                    return;
+                };
+
+                dir = parent.dir();
+                uncle = grandparent.child[@intFromEnum(mirror(dir))];
+                if (Node.color(uncle) == .black) {
+                    // case i5,i6
+                    // FIXME:
+                    if (node == parent.child[@intFromEnum(mirror(dir))]) {
+                        _ = self.rotateDir(parent, dir);
+                        node = parent;
+                        parent = grandparent.child[@intFromEnum(dir)].?;
+                    }
+                    // case i6
+                    _ = self.rotateDir(grandparent, mirror(dir));
+                    parent.color = .black;
+                    grandparent.color = .red;
+                    return;
+                }
+
+                // case i2
+                grandparent.color = .red;
+                parent.color = .black;
+                uncle.?.color = .black;
+
+                // iterate 2 tree levels higher
+                parent = node.parent orelse {
+                    break;
+                };
+            }
+
+            // case i3
+            return;
+        }
+    };
+}
+
+const Color = enum {
+    red,
+    black,
+};
+
+const Dir = enum(u8) {
+    left = 0,
+    right = 1,
+};
+
+fn dirFromOrder(o: std.math.Order) ?Dir {
+    return switch (o) {
+        .gt => .right,
+        .lt => .left,
+        .eq => null,
+    };
+}
+
+fn mirror(dir: Dir) Dir {
+    return @enumFromInt(1 - @intFromEnum(dir));
+}
