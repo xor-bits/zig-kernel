4224342e83c4b1a05f32bf7c7268d164989fbb6d
xor-bits <oneleh@utu.fi>
2025-05-08 01:57:12 +0300

    silence verbose logs

--- src/abi/conf.zig
+++ src/abi/conf.zig
@@ -1,9 +1,9 @@
-pub const LOG_EVERYTHING: bool = true;
+pub const LOG_EVERYTHING: bool = false;
 
-pub const LOG_SYSCALLS: bool = LOG_EVERYTHING or true;
-pub const LOG_OBJ_CALLS: bool = LOG_EVERYTHING or true;
-pub const LOG_CTX_SWITCHES: bool = LOG_EVERYTHING or true;
-pub const LOG_WAITING: bool = LOG_EVERYTHING or true;
-pub const LOG_SERVERS: bool = LOG_EVERYTHING or true;
+pub const LOG_SYSCALLS: bool = LOG_EVERYTHING or false;
+pub const LOG_OBJ_CALLS: bool = LOG_EVERYTHING or false;
+pub const LOG_CTX_SWITCHES: bool = LOG_EVERYTHING or false;
+pub const LOG_WAITING: bool = LOG_EVERYTHING or false;
+pub const LOG_SERVERS: bool = LOG_EVERYTHING or false;
 
-pub const KERNEL_PANIC_ON_USER_FAULT: bool = true;
+pub const KERNEL_PANIC_ON_USER_FAULT: bool = false;
